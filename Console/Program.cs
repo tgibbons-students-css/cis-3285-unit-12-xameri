dab7d57d0d688a072ae5ff6b6ca75ca5ea36674c
tgibbons-css <tgibbons@css.edu>
2016-11-22 09:56:23 -0600

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubtypeCovariance;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EntityRepository entityRepository = new EntityRepository();
+            Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
+
+            myEntity.Name = "Tom";
+            Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
+            Console.WriteLine("New entity Name: " + myEntity.Name);
+
+        }
+    }
+}
---
+++ SubtypeCovariance/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtypeCovariance
+{
+    public class User : Entity
+    {
+        public string EmailAddress { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
